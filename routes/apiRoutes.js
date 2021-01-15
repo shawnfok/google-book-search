c6f794dab776804750c28d161b0895b75c7f42a7
shawnfok <shawnfok@gmail.com>
2021-01-14 21:19:13 -0500

    built api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,7 @@
 const router = require("express").Router();
-const db = require("../models");
+const apiRoutes = require("./api");
 
-router.get("/recipes", (req, res) => {
-  // Use a regular expression to search titles for req.query.q
-  // using case insensitive match. https://docs.mongodb.com/manual/reference/operator/query/regex/index.html
-  db.Recipe.find({
-    title: { $regex: new RegExp(req.query.q, 'i')}
-  })
-    .then(recipes => res.json(recipes))
-    .catch(err => res.status(422).end());
-});
+// API Routes
+router.use("/api", apiRoutes);
 
 module.exports = router;
